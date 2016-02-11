86c30d3b0f4863d6aabcb4d8d89ba6f55d949759
Nimish <nimish@vinsol.com>
2016-02-11 18:02:58 +0530

    Seperate the admin controller and layouts in seperate namespace

--- apps/ex_shop/web/controllers/admin/home_controller.ex
+++ apps/ex_shop/web/controllers/admin/home_controller.ex
@@ -1,15 +1,10 @@
 defmodule ExShop.Admin.HomeController do
-  use ExShop.Web, :controller
+  use ExShop.Web, :admin_controller
 
-  plug Guardian.Plug.EnsureAuthenticated, handler: __MODULE__, key: :admin
+  plug Guardian.Plug.EnsureAuthenticated, handler: ExShop.Auth.HandleUnauthenticated, key: :admin
 
   def index(conn, _params) do
     render(conn, "index.html")
   end
 
-  def unauthenticated(conn, _params) do
-    conn
-    |> put_flash(:error, "Admin authentication required")
-    |> redirect(to: admin_session_path(conn, :new))
-  end
 end
