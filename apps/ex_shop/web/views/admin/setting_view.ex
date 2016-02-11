46c8f3460924a40c8df373e0c94dc5b88029d46f
Nimish <nimish@vinsol.com>
2016-02-11 12:35:23 +0530

    style fixes, add missing brackets

--- apps/ex_shop/web/views/admin/setting_view.ex
+++ apps/ex_shop/web/views/admin/setting_view.ex
@@ -1,5 +1,5 @@
 defmodule ExShop.Admin.SettingView do
-	use ExShop.Web, :view
+  use ExShop.Web, :view
 
   def setting_name(%Phoenix.HTML.Form{data: %ExShop.SettingPair{name: name}}) do
     Phoenix.Naming.humanize(name)
--- apps/ex_shop/web/views/admin/state_view.ex
+++ apps/ex_shop/web/views/admin/state_view.ex
@@ -1,5 +1,5 @@
 defmodule ExShop.Admin.StateView do
-	use ExShop.Web, :view
+  use ExShop.Web, :view
   def render("state.json", %{state: state}) do
     %{id: state.id, name: state.name, abbr: state.abbr}
   end
