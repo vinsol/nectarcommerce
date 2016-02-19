d1408c96e0fef1d50bb7557f168f653ae86f75ac
Pikender Sharma <avid.piks@gmail.com>
2016-02-19 13:19:56 +0530

    [Product Mgmt - Rough Edges] Show OptionTypes on Product Show when added Show Placeholder image, if no image uploaded yet

--- apps/ex_shop/priv/repo/seeds.exs
+++ apps/ex_shop/priv/repo/seeds.exs
@@ -9,7 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
-Seed.LoadCountry.seed!
-Seed.CreateZone.seed!
+#Seed.LoadCountry.seed!
+#Seed.CreateZone.seed!
 ExShop.Repo.insert!(%ExShop.User{name: "Admin", email: "admin@vinsol.com", encrypted_password: Comeonin.Bcrypt.hashpwsalt("vinsol"), is_admin: true})
-Seed.LoadSettings.seed!
+#Seed.LoadSettings.seed!
