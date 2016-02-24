344a4e40b87dce89e87ee5f15c318ec175fb409f
Pikender Sharma <avid.piks@gmail.com>
2016-02-24 20:02:55 +0530

    Renamed Migration clashing with create product to create dummy products Added override: true to phoenix_ecto to avoid dependency clash with arc_ecto
    Changed Product and Variant Controller to use new/dedicated Auth.Unauthenticated Module

--- apps/ex_shop/mix.exs
+++ apps/ex_shop/mix.exs
@@ -37,7 +37,7 @@ defmodule ExShop.Mixfile do
     [
       {:phoenix, "~> 1.1.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_ecto, "~> 2.0"},
+      {:phoenix_ecto, "~> 2.0", override: true},
       {:phoenix_html, "~> 2.4"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.9"},
