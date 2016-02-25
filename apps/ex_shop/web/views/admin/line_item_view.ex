55cc1c3516d2b94018e52aad5b70a071546d621a
Nimish <nimish@vinsol.com>
2016-02-25 12:21:56 +0530

    Display quantity in line item

--- apps/ex_shop/web/views/admin/line_item_view.ex
+++ apps/ex_shop/web/views/admin/line_item_view.ex
@@ -5,9 +5,17 @@ defmodule ExShop.Admin.LineItemView do
   def render("line_item.json", %{line_item: line_item}) do
     line_item = ExShop.Repo.get!(ExShop.LineItem, line_item.id) |> ExShop.Repo.preload([variant: :product])
     if line_item.variant.is_master do
-      %{id: line_item.id, quantity: line_item.quantity, variant: %{display_name: "#{line_item.variant.product.name}", id: line_item.variant.id}}
+      %{id: line_item.id,
+        quantity: line_item.quantity,
+        variant: %{
+          display_name: "#{line_item.variant.product.name}",
+          id: line_item.variant.id}}
     else
-      %{id: line_item.id, quantity: line_item.quantity, variant: %{display_name: "#{line_item.variant.product.name} (#{line_item.variant.sku})", id: line_item.variant.id}}
+      %{id: line_item.id,
+        quantity: line_item.quantity,
+        variant: %{
+          display_name: "#{line_item.variant.product.name} (#{line_item.variant.sku})",
+          id: line_item.variant.id}}
     end
   end
 
