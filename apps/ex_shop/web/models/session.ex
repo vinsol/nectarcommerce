a46f42ee58d479ced6ebd2907b657545b89f152c
Pikender Sharma <pikender@vinsol.com>
2016-03-08 20:36:00 +0530

    [Session Management] Refactored Admin Session Login   to add errors to changeset and show properly to user Pattern match on different conditions made it a smooth breeze Added error_tag to show email and password errors next to form fields

--- apps/ex_shop/web/models/session.ex
+++ apps/ex_shop/web/models/session.ex
@@ -1,18 +1,39 @@
 defmodule ExShop.Session do
   alias ExShop.User
+  alias Ecto.Changeset
 
-  def admin_login(params, repo) do
-    user = repo.get_by(User, email: String.downcase(params["email"]), is_admin: true)
-    case authenticate(user, params["password"]) do
-      true -> {:ok, user}
-      _    -> {:error, nil}
+  def admin_login(changeset = %{params: %{"email" => email}}, repo) when (email == "" or email == nil) do
+    changeset = changeset
+      |> Changeset.add_error(:email, "can't be blank")
+
+    {:error, changeset}
+  end
+
+  def admin_login(changeset = %{params: %{"password" => password}}, repo) when (password == "" or password == nil) do
+    changeset = changeset
+      |> Changeset.add_error(:password, "can't be blank")
+
+    {:error, changeset}
+  end
+
+  def admin_login(changeset = %{params: %{"email" => email, "password" => password}}, repo) do
+    user = repo.get_by(User, email: String.downcase(email), is_admin: true)
+    case authenticate(changeset, user, password) do
+      {:ok, user} -> {:ok, user}
+      {:error, changeset} -> {:error, changeset}
     end
   end
 
-  defp authenticate(user, password) do
-    case user do
-      nil -> false
-      _   -> Comeonin.Bcrypt.checkpw(password, user.encrypted_password)
+  defp authenticate(changeset, user, password) when user == nil do
+    changeset = changeset
+      |> Changeset.add_error(:email, "not found in records")
+    {:error, changeset}
+  end
+
+  defp authenticate(changeset, user, password) do
+    case Comeonin.Bcrypt.checkpw(password, user.encrypted_password) do
+      true -> {:ok, user}
+      false -> {:error, changeset |> Changeset.add_error(:password, "not matched with records")}
     end
   end
 end
