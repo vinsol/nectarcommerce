266b73fd3b33295659d028b6e475d3f03440daf8
Pikender Sharma <pikender@vinsol.com>
2016-03-11 12:38:46 +0530

    [Search] Added search form on Admin Product Index Changed AdminProductController to use search than all Products Added SearchProduct Module to avail form changeset and search logic [Search] Added to search by description when description passed Added index action to pattern match for search parameters   when no search params fallback to all Products Added /cover to .gitignore of ex_shop and worldly apps

---
+++ apps/ex_shop/web/models/search/product.ex
@@ -0,0 +1,42 @@
+defmodule ExShop.SearchProduct do
+  use ExShop.Web, :model
+
+  alias ExShop.Product
+
+  schema "abstract table:search_product" do
+    field :name, :string, virtual: true
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+      |> cast(params, ~w(), ~w(name))
+  end
+
+  def search(params) do
+    Product
+      |> search_name(params)
+      |> search_description(params)
+  end
+
+  defp search_name(queryable, %{"name" => name}) when (is_nil(name) or name == "") do
+    queryable
+  end
+  defp search_name(queryable, %{"name" => name}) do
+    from p in queryable,
+      where: ilike(p.name, ^("%#{name}%"))
+  end
+  defp search_name(queryable, params) do
+    queryable
+  end
+
+  defp search_description(queryable, %{"description" => description}) when (is_nil(description) or description == "") do
+    queryable
+  end
+  defp search_description(queryable, %{"description" => description}) do
+    from p in queryable,
+      where: ilike(p.description, ^("%#{description}%"))
+  end
+  defp search_description(queryable, params) do
+    queryable
+  end
+end
