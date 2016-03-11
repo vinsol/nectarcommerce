27d137c5776b74432af1ce08ff39213f1d539b46
Nimish <nimish@vinsol.com>
2016-03-11 14:18:48 +0530

    add index for product categories

--- apps/ex_shop/priv/repo/migrations/20160309122744_create_product_category.exs
+++ apps/ex_shop/priv/repo/migrations/20160309122744_create_product_category.exs
@@ -7,6 +7,6 @@ defmodule ExShop.Repo.Migrations.CreateProductCategory do
       add :product_id, references(:products)
       timestamps
     end
-
+    create unique_index(:product_categories, [:category_id, :product_id])
   end
 end
