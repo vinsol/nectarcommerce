a25cf19357f0b71405c15e9a694715fa8e782050
Nimish <nimish@vinsol.com>
2016-03-14 16:00:09 +0530

    provide search changesets to category listing pages as well

--- apps/ex_shop/web/controllers/admin/checkout_controller.ex
+++ apps/ex_shop/web/controllers/admin/checkout_controller.ex
@@ -2,6 +2,7 @@ defmodule ExShop.Admin.CheckoutController do
   use ExShop.Web, :admin_controller
 
   plug Guardian.Plug.EnsureAuthenticated, handler: ExShop.Auth.HandleAdminUnauthenticated, key: :admin
+  plug :go_back_to_cart_if_empty when action in [:checkout, :next, :back]
 
   alias ExShop.CheckoutManager
   alias ExShop.Order
@@ -27,6 +28,17 @@ defmodule ExShop.Admin.CheckoutController do
     case CheckoutManager.back(order) do
       {:ok, updated_order} ->
         redirect(conn, to: admin_order_checkout_path(conn, :checkout, order))
+    end
+  end
+
+  def go_back_to_cart_if_empty(conn, _params) do
+    order = Repo.get!(Order, conn.params["order_id"])
+    if ExShop.Order.cart_empty? order do
+      conn
+      |> put_flash(:error, "please add some products to cart before continuing")
+      |> redirect(to: admin_cart_path(conn, :edit, order))
+    else
+      conn
     end
   end
 
