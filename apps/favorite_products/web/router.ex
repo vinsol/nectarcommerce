520f13e017cd52114fb743e00164f57905f9fca2
Pikender Sharma <pikender@vinsol.com>
2016-03-30 19:55:38 +0530

    Reused routes as extended to Nectar.Router

--- apps/favorite_products/web/router.ex
+++ apps/favorite_products/web/router.ex
@@ -17,7 +17,6 @@ defmodule FavoriteProducts.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/favorites", FavoriteController
   end
 
   # Other scopes may use custom stacks.
