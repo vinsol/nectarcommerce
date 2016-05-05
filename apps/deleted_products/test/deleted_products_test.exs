ac14b345eb233b1db19e9f52bf22f194184a58bb
Nimish <nimish@vinsol.com>
2016-05-05 14:24:44 +0530

    add test cases for deleted products

--- apps/deleted_products/test/deleted_products_test.exs
+++ apps/deleted_products/test/deleted_products_test.exs
@@ -1,8 +1,16 @@
 defmodule DeletedProductsTest do
   use ExUnit.Case
-  doctest DeletedProducts
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "added field to Nectar.Product succesfully" do
+    assert Enum.member? Nectar.Product.__schema__(:fields), :deleted
+  end
+
+  test "added associations to Nectar.Product succesfully" do
+    assert Enum.member? Nectar.Product.__schema__(:associations), :deleted_by
+  end
+
+  test "added methods to Nectar.Product succesfully" do
+    assert Enum.member? Nectar.Product.__info__(:functions), {:mark_as_deleted_changeset, 2}
+    assert Enum.member? Nectar.Product.__info__(:functions), {:deleted_products, 0}
   end
 end
