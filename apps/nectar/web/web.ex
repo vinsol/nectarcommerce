8546b5688926f7ec1e95b0c793e420cee29dbc27
Nimish <nimish@vinsol.com>
2016-05-10 15:02:22 +0530

    add support for extending views by providing partials

--- apps/nectar/web/web.ex
+++ apps/nectar/web/web.ex
@@ -56,11 +56,11 @@ defmodule Nectar.Web do
 
   def view do
     quote do
+      use Nectar.Extender # ensure it is first import, since Phoenix.View will create some methods due to which it will not be matchable
       use Phoenix.View, root: "web/templates"
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
-
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
