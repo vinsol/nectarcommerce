7c5b051c7417122a38babf748bdd1d7243733ad4
Nimish <nimish@vinsol.com>
2016-05-16 14:06:30 +0530

    make stacked alerts and dismissable alerts dumber

--- apps/nectar/web/static/js/components/dismissable_alert.js
+++ apps/nectar/web/static/js/components/dismissable_alert.js
@@ -2,18 +2,9 @@ import React from "react";
 
 export default class DismissableAlert extends React.Component {
 
-  constructor() {
-    super();
-    this.dismiss = this.dismiss.bind(this);
-  }
-
-  dismiss() {
-    this.props.clear(this.props.index);
-  }
-
   render() {
     return (<div className="alert alert-danger">
-            <a href="#" class="close" title="close" onClick={this.dismiss}>×</a>
+            <a href="#" class="close" title="close" onClick={this.props.dismiss}>×</a>
               <span>{this.props.message}</span>
             </div>);
   }
--- apps/nectar/web/static/js/components/stacked_alerts.js
+++ apps/nectar/web/static/js/components/stacked_alerts.js
@@ -3,7 +3,11 @@ import DismissableAlert from "./dismissable_alert";
 
 export default class StackedAlerts extends React.Component {
   render() {
-    const alerts = this.props.alerts.map( (alert, idx) => <DismissableAlert message={alert.message} key={idx} index={idx} clear={this.props.clear}/>);
+    const alerts = this.props.alerts.map( (alert, idx) => <DismissableAlert
+                                                             message={alert.message}
+                                                             key={idx}
+                                                             index={idx}
+                                                             dismiss={this.props.clear.bind(this, idx)}/>);
     return (
       <div>
         {alerts}
