77e4571c6766a733f6b8c0c125b1369c8b8ab968
Nimish <nimish@vinsol.com>
2016-05-30 12:33:32 +0530

    implement refunds and capturing braintree payments

--- apps/nectar/web/gateways/braintree_impl.ex
+++ apps/nectar/web/gateways/braintree_impl.ex
@@ -22,4 +22,22 @@ defmodule Nectar.Billing.Gateways.BraintreeImpl do
     {:ok, "transaction successful"}
   end
 
+  def capture(transaction_id, opts) do
+    capture_amount = Keyword.get(opts, :amount)
+    if capture_amount do
+      Transaction.submit_for_settlement(transaction_id, %{amount: capture_amount})
+    else
+      Transaction.submit_for_settlement(transaction_id, %{})
+    end
+  end
+
+  def refund(transaction_id, opts) do
+    refund_amount = Keyword.get(opts, :amount)
+    if refund_amount do
+      Transaction.refund(transaction_id, %{amount: refund_amount})
+    else
+      Transaction.refund(transaction_id, %{})
+    end
+  end
+
 end
