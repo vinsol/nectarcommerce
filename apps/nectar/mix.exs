d1f8b38bd268b227d081150b03a1ff8e0845cdb9
Nimish <nimish@vinsol.com>
2016-07-11 17:38:02 +0530

    upgrade phoenix version

--- apps/nectar/mix.exs
+++ apps/nectar/mix.exs
@@ -26,7 +26,7 @@ defmodule Nectar.Mixfile do
       applications: [
         :phoenix, :phoenix_html, :cowboy, :logger, :gettext,
         :phoenix_ecto, :postgrex, :worldly, :yamerl, :commerce_billing, :braintree,
-        :ex_aws, :httpoison, :yamerl, :worldly
+        :ex_aws, :httpoison, :phoenix_pubsub
       ]
     ]
   end
@@ -40,9 +40,10 @@ defmodule Nectar.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.1.4"},
+      {:phoenix, "~> 1.2.0"},
+      {:phoenix_pubsub, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_ecto, "~> 2.0", override: true},
+      {:phoenix_ecto, "~> 3.0"},
       {:phoenix_html, "~> 2.4"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.9"},
