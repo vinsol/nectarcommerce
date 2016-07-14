b1b5198c769cb36b4d2635a0adcb7c51b9f811d7
Nimish <nimish@vinsol.com>
2016-07-14 19:45:49 +0530

    Use the commands for updating payments and setting

--- apps/nectar/web/commands/payment_method.ex
+++ apps/nectar/web/commands/payment_method.ex
@@ -2,13 +2,25 @@ defmodule Nectar.Command.PaymentMethod do
   use Nectar.Command, model: Nectar.PaymentMethod
   import Ecto.Query
 
-  def enable(repo, payment_method_ids) do
+  def make_active_enabled_and_disable_other(repo, params) do
+    enabled_payment_method_ids =
+      Enum.filter(params, fn
+        ({_, %{"enabled" => "true"}}) -> true
+        ({_, %{"enabled" => "false"}}) -> false
+      end) |> Enum.map(fn({_, %{"id" => id}}) -> id end)
+    repo.transaction(fn ->
+      repo.update_all(enable(enabled_payment_method_ids), [])
+      repo.update_all(disable_other_than(enabled_payment_method_ids), [])
+    end)
+  end
+
+  defp enable(payment_method_ids) do
     from payment in Nectar.PaymentMethod,
     where: payment.id in ^payment_method_ids,
     update: [set: [enabled: true]]
   end
 
-  def disable_other_than(repo, payment_method_ids) do
+  defp disable_other_than(payment_method_ids) do
     from payment in Nectar.PaymentMethod,
     where: not payment.id in ^payment_method_ids,
     update: [set: [enabled: false]]
